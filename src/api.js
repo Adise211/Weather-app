9da977e1a99e42779c0469457281ad340e17bd92
Adise211 <101215614+Adise211@users.noreply.github.com>
2023-01-18 09:23:04 +0200

    forecast api function

--- src/api.js
+++ src/api.js
@@ -18,3 +18,23 @@ export const searchCities = async (value) => {
         console.log("Error fetching cities",error);
     }
 };
+
+
+export const forecastByDays = async (days, cityKey) => {
+    try {
+        const response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/${days}day/${cityKey}?apikey=${API_KEY}`,
+            {
+                method: 'GET',
+                headers: {
+                    "Access-Control-Request-Method": "GET",
+                    "Access-Control-Request-Headers": "Content-Type, Accept",
+                }
+            }
+        );
+        const forecast = await response.json();
+        return forecast;
+
+    } catch (error) {
+        console.log("Error fetching weather details",error);
+    }
+};
