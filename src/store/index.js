a6bf16a474d5dec3effebc2f0e261f786a0ee634
Adise211 <101215614+Adise211@users.noreply.github.com>
2023-01-18 09:31:03 +0200

    install and add vuex

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+    state() {
+        return {
+            forecast: null
+        }
+    },
+    mutations: {},
+    actions: {},
+    getters: {}
+});
+
+export default store;
