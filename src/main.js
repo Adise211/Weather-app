6f61e6954ebab20d773d34f4e5914c076fe31da2
Adise211 <101215614+Adise211@users.noreply.github.com>
2023-01-22 08:33:48 +0200

    installed vuetify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
-// import Vuex from 'vuex';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false
 
 
 new Vue({
+  Vuetify,
   store,
   router,
   render: h => h(App)
